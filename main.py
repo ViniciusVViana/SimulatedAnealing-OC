ac017d78db13be02f56a7adb289e3677eb86ec36
Vinicius.Viana <viniciusvianav1099@gmail.com>
2024-05-19 00:32:15 -0300

    refactor: optimize code for calculating minimum values in main.py

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ initial_solution = [list() * i for i in range(simulatedAnealing.num_prog)]
 
 min_value = []
 for mod in range(simulatedAnealing.num_modules):
-   min_value.append(min(row[mod] for row in simulatedAnealing.prog_hour_cost))
-    
-second_min_value = []
-for mod in range(simulatedAnealing.num_modules):
-    secondi
-
+     min_value.append(min(row[mod] for row in simulatedAnealing.prog_hour_cost))
 
 print(min_value)
+
+""" second_min_value = []
+aux = simulatedAnealing.prog_hour_cost
+for mod in range(simulatedAnealing.num_modules):
+    while True:                                      SOU UM INCOMPETENTE!!!!
+        value = min(row[mod] for row in aux)
+        if(value == min_value[mod]): 
+
+print(value) """
